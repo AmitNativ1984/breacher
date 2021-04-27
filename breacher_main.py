3136d3f4591422f63f775542f0d3f11478329fbb
AmitNativ1984 <amitnativ@gmail.com>
2021-04-27 10:58:26 +0000

    working version with connected componnets or area

--- breacher_main.py
+++ breacher_main.py
@@ -3,6 +3,11 @@ import os
 import cv2
 import argparse
 import numpy as np
+import logging
+FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+logger = logging.getLogger("depth_breacher")
+
 import threading
 from distutils.util import strtobool
 
@@ -26,58 +31,69 @@ class DepthBreacher(object):
     
     def __init__(self):
         print('DepthBreacher instance created')
-        self.distance2hatch=None
+        self.distanceFromSurface=None
         self.noise=100 #[mm] depth noise
         self.detections = None
-        self.distance2hatch = 3000
-        self.breachDepthUncertainty = 500
-        
-    def getDistanceCallback(self, distance):
-        self.distance2hatch = distance
-    
-    def getDetectionsCallback(self, detections):
-        self.detections = detections.detections
+        self.distanceFromSurface = 3000
+        self.wallThickness = 500
+        self.depth = None
+        self.breach_zone = None
+        self.pointingFinger = None
 
-    def getDetectionImageCallback(self, img):
-        self.detectionImage = bridge.imgmsg_to_cv2(img, desired_encoding='passthrough')
-        self.detectionImage = self.detectionImage.astype(np.uint8)
+        self.mode = 'area'#'pointingFinger'
+
+    def getDistanceCallback(self, distance):
+        self.distanceFromSurface = distance
 
     def getPointFingerCallback(self, pointingFinger):
         self.pointingFinger = pointingFinger.point
         self.pointingFinger.x = int(self.pointingFinger.x)
         self.pointingFinger.y = int(self.pointingFinger.y)
-
-    def getActiveBbox(self, cls_id = 1):
-        for box_ind, detection in enumerate(self.detections): 
-            # && cls_id
-            if detection.results[0].id != cls_id:
-                continue
-
-            bxmin = int(detection.bbox.center.x - detection.bbox.size_x / 2)
-            bymin = int(detection.bbox.center.y - detection.bbox.size_y / 2)
-            bxmax = int(detection.bbox.center.x + detection.bbox.size_x / 2)
-            bymax = int(detection.bbox.center.y + detection.bbox.size_y / 2)
-            bymax = int(detection.bbox.center.y + detection.bbox.size_y / 2)
-
-            if self.pointingFinger.x >= bxmin and self.pointingFinger.x <= bxmax and self.pointingFinger.y >= bymin and self.pointingFinger.y <= bymax:
-                return box_ind
-               
-        return -1
     
-    def filterActiveBoxBreachZone(self, breach_zone):
-        bbox_ind = self.getActiveBbox()
-        bbox = self.detections[bbox_ind].bbox
-        xmin = int(bbox.center.x - bbox.size_x)
-        ymin = int(bbox.center.y - bbox.size_y)
-        xmax = int(bbox.center.x + bbox.size_x)
-        ymax = int(bbox.center.y + bbox.size_y)
+    def distance2breach(self):
+        return self.distanceFromSurface + self.noise + self.wallThickness
+    
+    def getDepthROI(self):
+        depthROI = np.zeros_like(self.depth)
+        depthROI[self.depth > self.distance2breach()] = 1
+        depthROI = depthROI.astype(np.uint8)
+        kernel = np.ones((5, 5), np.uint8)
+        depthROI = cv2.morphologyEx(depthROI, cv2.MORPH_CLOSE, kernel)
+        kernel = np.ones((9,9), np.uint8)
+        depthROI = cv2.morphologyEx(depthROI, cv2.MORPH_CLOSE, kernel)
+        return depthROI
 
-        breach_zone[:ymin, :] = 0
-        breach_zone[ymax:, :] = 0
-        breach_zone[:, :xmin] = 0
-        breach_zone[:, xmax:] = 0
+    
+    def getBreachAreaID(self, depthROI, cc_img, n_labels, stats):
+        if self.mode == 'pointingFinger':
+            if self.pointingFinger is None:
+                print('no pointingFinger recived')
+                return -1
+   
+            if depthROI[self.pointingFinger.y, self.pointingFinger.x] == 0:
+                print('pointing fingure not pointing to area with depth valid for breach')
+                return -1
 
-        return breach_zone
+            else:
+                label_id = cc_img[self.pointingFinger.y, self.pointingFinger.x]
+            
+        elif self.mode =='area':
+            # breach area is assumed to be the largest
+            label_area = -1 * np.ones((n_labels))
+            for label, stat in enumerate(stats):
+                if depthROI[cc_img == label][0] > 0:
+                    label_area[label] = stat[-1]
+
+            label_id = np.argmax(label_area)
+
+            if label_id == -1:
+                print("could not detect area with depth valid for breach according to area size")
+           
+        return label_id
+    
+    def getDetectionImage(self, detectionImage):
+        self.detectionImage = bridge.imgmsg_to_cv2(detectionImage, desired_encoding='passthrough')
+        self.detectionImage = self.detectionImage.astype(np.uint8)
 
     def depthImageCallback(self, depthImage):
         """
@@ -87,61 +103,43 @@ class DepthBreacher(object):
         """
 
         timeStamp = depthImage.header.stamp
-        depth = bridge.imgmsg_to_cv2(depthImage, desired_encoding='passthrough')
-        # segmenting image
-        breach_zone = np.zeros_like(depth)
-        breach_zone[depth > self.distance2hatch + self.noise + self.breachDepthUncertainty] = 1
-        
-        breach_zone = breach_zone.astype(np.uint8)
+        self.depth = bridge.imgmsg_to_cv2(depthImage, desired_encoding='passthrough')       
 
-        try:
-            self.detectionImageOut = self.detectionImage.copy()
-        except Exception:
-            print("no active detections, can't crossvalidate with pointing finger")
+        depthROI = self.getDepthROI()
+        n_labels, cc_img, stats, centroids = cv2.connectedComponentsWithStats(depthROI, connectivity=8)
+        cc_img = np.array(cc_img).astype(np.uint8)
+                
+        label_id = self.getBreachAreaID(depthROI, cc_img, n_labels, stats)
+        if label_id == -1:
             return
         
-        # breach_zone = self.filterActiveBoxBreachZone(breach_zone)       
-        self.detectionImageOut[breach_zone == 1, 0] = 255
+        breach_zone = np.zeros_like(cc_img)
+        breach_zone[cc_img == label_id] = 1
+
+        breach_point = centroids[label_id]
+      
+      
+        breachPoint = PointStamped()
+        breachPoint.header.stamp = timeStamp
+        breachPoint.header.frame_id = 'd415_aligned_depth_to_color'
+        breachPoint.point.x = centroids[label_id, 0]
+        breachPoint.point.y = centroids[label_id, 1]
+        breachPoint.point.z = self.distance2breach()
+
         
-        contours, hierarchy = cv2.findContours(breach_zone,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        breach_zone = cv2.cvtColor(breach_zone,cv2.COLOR_GRAY2RGB)
-        cv2.drawContours(self.detectionImageOut, contours, -1, (255,0,0), 3)
-        area=[]
-        cX=[]
-        cY=[]
-        for contour in contours:
-            # calculate moments for each contour
-            M = cv2.moments(contour)
-            # calculate x,y coordinate of center
-            area.append(cv2.contourArea(contour))
-            cX.append(int(M["m10"] / M["m00"]))
-            cY.append(int(M["m01"] / M["m00"]))
-
-        if len(cX) > 0:
-            # this is where crossing with window bbox will take place
-            cX = cX[0]
-            cY = cY[0]  
+        # publish results
+        breachPointPublisher.publish(breachPoint)
+        breachImagePublisher.publish(bridge.cv2_to_imgmsg(cv2.cvtColor(breach_zone * 255, cv2.COLOR_BGR2RGB), encoding="passthrough"))
         
-            breachPoint = PointStamped()
-            breachPoint.header.stamp = timeStamp
-            breachPoint.header.frame_id = 'd415_aligned_depth_to_color'
-            breachPoint.point.x = cX
-            breachPoint.point.y = cY
-            breachPoint.point.z = self.distance2hatch
-            
-            cv2.circle(breach_zone, (breachPoint.point.x, breachPoint.point.y), 5, (255, 0, 0), -1)
-
-            for detection in self.detections:
-                cv2.circle(breach_zone, (int(detection.bbox.center.x), int(detection.bbox.center.y)), 5, (0, 255, 0), -1)
-
-            cv2.circle(self.detectionImageOut, (self.pointingFinger.x, self.pointingFinger.y), 5, (0, 255, 0), -1)
-
-            # publish results
-            breachPointPublisher.publish(breachPoint)
-            breachImagePublisher.publish(bridge.cv2_to_imgmsg(cv2.cvtColor(breach_zone, cv2.COLOR_BGR2RGB), encoding="passthrough"))
-            detectionImagePublisher.publish(bridge.cv2_to_imgmsg(cv2.cvtColor(self.detectionImageOut, cv2.COLOR_BGR2RGB), encoding="passthrough"))
-            print("breachPoint (x,y,z)=({}, {}, {})".format(breachPoint.point.x, breachPoint.point.y, breachPoint.point.z))
+        try:
+            self.detectionImage[breach_zone == 1, 0] = 255
+            cv2.circle(self.detectionImage, (int(breachPoint.point.x), int(breachPoint.point.y)), 5, (0, 255, 0), -1)
+        except Exception:
+            pass
         
+        detectionImagePublisher.publish(bridge.cv2_to_imgmsg(cv2.cvtColor(self.detectionImage, cv2.COLOR_BGR2RGB), encoding="passthrough"))
+        print("breachPoint (x,y,z)=({}, {}, {})".format(breachPoint.point.x, breachPoint.point.y, breachPoint.point.z))
+    
         return
 
 
@@ -150,8 +148,6 @@ if __name__=="__main__":
     
     rospy.init_node('hatch_detector', anonymous=True)
     rospy.Subscriber("/d415/aligned_depth_to_color/image_raw", Image, depth_breacher.depthImageCallback, queue_size=1, buff_size=2 ** 24)  
-    rospy.Subscriber("/d415/aligned_depth_to_color/image_raw", Image, depth_breacher.depthImageCallback, queue_size=1, buff_size=2 ** 24)  
-    rospy.Subscriber("/seeker/detectionImage", Image, depth_breacher.getDetectionImageCallback, queue_size = 10)
-    rospy.Subscriber("/seeker/detections", Detection2DArray, depth_breacher.getDetectionsCallback, queue_size = 10)
     rospy.Subscriber("/pointingfinger/TargetPoint", PointStamped, depth_breacher.getPointFingerCallback, queue_size = 10)
+    rospy.Subscriber("/d415/color/image_raw", Image, depth_breacher.getDetectionImage, queue_size=1, buff_size=2 ** 24)
     rospy.spin()
